b9353717a3a7e2fa675279d2d0fc06b9e2b0cc5e
Cássio Queiroz Pereira <145166500+cassioqueirozpereira@users.noreply.github.com>
2024-11-23 10:38:51 -0300

    teste

--- pai.c
+++ pai.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
-#include <string.h>
-#include <locale.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-int main () {
-	setlocale(LC_ALL, "Portuguese");
-	
-	char nome[10];
-	
-	printf("Digite seu nome: ");
-	scanf("%s", nome);
-	puts(nome);
+int main() {
+int resultado, pid, ppid;
+resultado = fork();
+if (resultado < 0)
+printf("Algo deu errado!!!\n");
+pid = getpid();
+if (resultado == 0) {
+ppid = getppid();
+printf("Eu sou o processo filho, meu PID é %d e meu pai tem PID=%d.\n", pid, ppid);
+}
+if (resultado > 0) {
+printf("Eu sou o processo pai, meu PID é %d e meu filho tem PID=%d.\n", pid, resultado);
+waitpid(resultado, NULL, 0);
+}
 }
